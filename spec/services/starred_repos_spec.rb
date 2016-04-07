4c1b9886be200c26d376261dbbb644fa1242d2bf
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-07 15:18:38 -0600

    Add starred repos test

---
+++ spec/services/starred_repos_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe NickHub do
+  context "#starred_repos" do
+    it "returns starred repos" do
+      VCR.use_cassette("nick_hub#starred_repos") do
+        service = NickHub.new
+        user = User.create(username: "NickyBobby")
+        starred_repos = service.get_starred_repos
+        starred_repo = starred_repos.first[:owner]
+        expect(starred_repo[:login]).to eq("ShannonPaige")
+        expect(starred_repos.first[:name]).to eq("comic-db")
+        expect(starred_repos.count).to eq(1)
+      end
+    end
+  end
+end
